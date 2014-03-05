575cfab33454a10375f28f7c4843243a3f363caf
johnduro <https://johnduro@bitbucket.org>
2014-03-05 01:57:29 +0100

    debut de resolution

--- lem_in.c
+++ lem_in.c
@@ -359,20 +359,118 @@ t_res		*init_res(void)
 	return (new);
 }
 
-void		make_start(t_mz *start)
+t_connex		*copy_connex(t_connex *list)
 {
+	t_connex	*first;
+	t_connex	*ptr;
+	t_connex	*browse;
+
+	first = init_connex(list->name);
+	browse = list->next;
+	ptr = first;
+	while (browse)
+	{
+		ptr->next = init_connex(browse->name);
+		ptr = ptr->next;
+		browse = browse->next;
+	}
+	return (first);
+}
+
+t_connex		*make_start(t_mz *start)
+{
+	t_connex	*copy;
+
 	start->flag = 1;
-	start->
+	start->path = 1;
+	copy = copy_connex(start->list);
+	return (copy);
+}
+
+void		cat_connex(t_res *res, t_mz *room)
+{
+	t_connex	*connex;
+
+	if (res->next == NULL)
+		res->next = copy_connex(room->list);
+	else
+	{
+		connex = res->next;
+		while (connex->next)
+			connex = connex->next;
+		connex->next = copy_connex(room->list);
+	}
+}
+
+int			find_solution(t_mz *maze, t_res *res)
+{
+	t_connex		*connex;
+	t_mz			*ptr;
+	int				ret;
+	static int		i = 2;
+
+	ret = 0;
+	connex = res->current;
+	while (connex)
+	{
+		ptr = maze->rooms;
+		while (ptr)
+		{
+			if (!ft_strcmp(ptr->name, connex->name) && ptr->flag == 0)
+			{
+				if (ptr->end == 1)
+					ret = 1;
+				cat_connex(res, ptr);
+				ptr->flag = 1;
+				ptr->path = i;
+			}
+			ptr = ptr->next;
+		}
+		connex = connex->next;
+	}
+	manage_res(res); //free et switch des lists a faire
+	i++;
+	return (ret);
+}
+
+int			is_solution(t_mz *rooms)
+{
+	t_mz		*tmp;
+	static int	last = 0;
+	int			i;
+
+	i = 0;
+	tmp = rooms;
+	while (tmp)
+	{
+		if (tmp->flag == 0)
+			i++;
+		tmp = tmp->next;
+	}
+	if (i == last)
+		return (1);
+	last = i;
+	return (0);
 }
 
 t_res		*treat_maze(t_env *maze)
 {
 	t_res	*res;
-	t_mz	*room;
+//	t_mz	*room;
+	int		find;
 
+	find = 0;
 	res = init_res();
-	make_start(maze->start_ptr);
-	room = maze->start_ptr;
+	res->current = make_start(maze->start_ptr);
+//	room = maze->start_ptr;
+	while (!find)
+	{
+		if (is_solution(maze->rooms))
+			error_lemmin();
+		find = find_solution(maze, res);
+	}
+	save_solution(res); // a faire
+	return (res);
 }
 
 int			main(void)
